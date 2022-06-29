5c469b68c19ea8643096000c5c2398c411a7bb9b
Gloria <gloghe@yahoo.it>
2022-06-29 15:13:36 +0200

    milestone due

---
+++ assets/script.js
@@ -0,0 +1,10 @@
+new Vue({
+	el: '#app',
+	data: {
+		dischi: [],
+	},
+	created() {
+		axios.get('http://localhost:80/php-ajax-dischi/api.php')
+			.then(response => this.dischi = response.data);
+	}
+})
--- index_iniziale.php
+++ index_iniziale.php
@@ -18,6 +18,10 @@ include_once __DIR__ . '/db/data.php';
     <section id="app"><?php
         include __DIR__ . '/includes/mainDischi.php';
     ?></section>
+
     
+    <script src="https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+	<script src="assets/script.js"></script>
 </body>
 </html>
