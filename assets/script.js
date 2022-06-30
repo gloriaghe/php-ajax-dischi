97388d14d14391d18d75e12061e73ad852d16132
Gloria <gloghe@yahoo.it>
2022-06-30 10:07:09 +0200

    divisione mailstone

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,15 @@
 new Vue({
-	el: '#app',
-	data: {
-		dischi: [],
-	},
-	created() {
-		axios.get('http://localhost:80/php-ajax-dischi/api.php')
-			.then(response => this.dischi = response.data);
-	}
+    el: '#app',
+    data: {
+        dischi: [],
+    },
+    created() {
+        axios.get('http://localhost:80/php-ajax-dischi/api.php')
+            .then(response => this.dischi = response.data);
+        
+    },
+    methods: {
+       
+    }
+
 })
--- mailstoneUno/index_iniziale.php
+++ mailstoneUno/index_iniziale.php
@@ -12,16 +12,23 @@ include_once __DIR__ . '/db/data.php';
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Dischi</title>
-	<link rel="stylesheet" href="assets/style.css">
+	<!-- font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+	<link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <section id="app"><?php
-        include __DIR__ . '/includes/mainDischi.php';
-    ?></section>
+    <section id="app">
+
+		<header>
+			<i class="fab fa-spotify" id="iconaSpotify"></i>
+		</header><?php
+		
+		include __DIR__ . '/includes/mainDischi.php';
+
+	?></section>
 
     
     <script src="https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.js"></script>
 	<script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
-	<script src="assets/script.js"></script>
 </body>
 </html>
